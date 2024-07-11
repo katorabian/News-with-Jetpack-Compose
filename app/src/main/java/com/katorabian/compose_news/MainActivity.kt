bc50207b285279a7fb213272d087f88942f799f4
katorabian <katorabian@gmail.com>
2024-07-11 17:36:23 +0700

    update(compose): 1st own composable fun

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -11,13 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import com.katorabian.compose_news.ui.theme.ComposeNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Android!")
+            UserInfo(name = "Alex", age = 20)
         }
     }
+}
+
+@Composable
+fun UserInfo(name: String, age: Int) {
+    Text("Hello, $name!\nyou are $age years old")
 }
