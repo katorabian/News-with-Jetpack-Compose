a9937ddeb2fced8342fb6fd013da02f1fa528193
katorabian <katorabian@gmail.com>
2024-07-14 01:11:23 +0700

    feat(compose): 1st use of Card fun in compose

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -3,22 +3,16 @@ package com.katorabian.compose_news
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,5 +25,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstagramProfileCard()
         }
+    }
+}
+
+@Preview
+@Composable
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp,
+            bottomEnd = 0.dp,
+            bottomStart = 0.dp
+        ),
+        colors = CardColors(
+            containerColor = Color.Green,
+            contentColor = Color.White,
+            Color.Transparent, Color.Transparent
+        ),
+        border = BorderStroke(1.dp, Color.Red)
+    ) {
+        Text(
+            text = "Hello World!",
+            Modifier.padding(8.dp)
+        )
     }
 }
