fb4e1bb518d3f3b9223aaf982c1d3d052736e580
katorabian <katorabian@gmail.com>
2024-07-13 13:36:11 +0700

    update(compose): build raw layout

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,42 +23,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.katorabian.compose_news.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun TimesTable() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        for (column in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1F)
-            ) {
-                for (row in 1 until 10) {
-                    val value = column * row
-                    val even = (column + row) % 2 == 0
-                    val color = if (even) Color.Yellow else Color.White
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1F)
-                            .border(width = 1.dp, color = Color.DarkGray)
-                            .background(color = color),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = value.toString())
-                    }
-                }
-            }
+            InstagramProfileCard()
         }
     }
 }
---
+++ app/src/main/java/com/katorabian/compose_news/ui/theme/TimesTable.kt
@@ -0,0 +1,47 @@
+package com.katorabian.compose_news.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun TimesTable() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        for (column in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1F)
+            ) {
+                for (row in 1 until 10) {
+                    val value = column * row
+                    val even = (column + row) % 2 == 0
+                    val color = if (even) Color.Yellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1F)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color = color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = value.toString())
+                    }
+                }
+            }
+        }
+    }
+}
