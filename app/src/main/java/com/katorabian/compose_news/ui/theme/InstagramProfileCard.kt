1377659e4274bcd9ad83fdc85ac39f204a01bf6e
katorabian <katorabian@gmail.com>
2024-07-15 23:15:37 +0700

    update(compose): add Text to activity

--- app/src/main/java/com/katorabian/compose_news/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/katorabian/compose_news/ui/theme/InstagramProfileCard.kt
@@ -2,7 +2,6 @@ package com.katorabian.compose_news.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,12 +14,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun InstagramProfileCard() {
@@ -46,27 +49,32 @@ fun InstagramProfileCard() {
                 .size(50.dp)
                 .background(color = Color.Cyan)
             )
-            TwoLines()
-            TwoLines()
-            TwoLines()
+            UserStatistics(title = "Posts", value = "6,950")
+            UserStatistics(title = "Followers", value = "436M")
+            UserStatistics(title = "Following", value = "76")
         }
     }
 }
 
 @Composable
-private fun TwoLines() {
+private fun UserStatistics(
+    title: String,
+    value: String
+) {
     Column(
         modifier = Modifier.height(80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly,
     ) {
-        Box(modifier = Modifier
-            .size(25.dp)
-            .background(color = Color.Blue)
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive
         )
-        Box(modifier = Modifier
-            .size(25.dp)
-            .background(color = Color.DarkGray)
+        Text(
+            text = title,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold
         )
     }
 }
