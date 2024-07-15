c7c2e1c352c09c408611784f0ab071b8680c9cfd
katorabian <katorabian@gmail.com>
2024-07-16 02:04:44 +0700

    feat(compose): try Image fun

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -3,13 +3,19 @@ package com.katorabian.compose_news
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
@@ -42,33 +48,10 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun TestText() {
-    Text(
-        text = buildAnnotatedString {
-            withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
-                append("Hello")
-            }
-            withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
-                append(" ")
-            }
-            withStyle(
-                SpanStyle(
-                    fontFamily = FontFamily.Cursive,
-                    fontSize = 30.sp,
-                    textDecoration = TextDecoration.LineThrough
-                )
-            ) {
-                append("World")
-                append("!")
-            }
-            append("\n OTHER WORDS")
-        },
-        // Накладывается на весь текст выше вставленный текст тоже
-//        fontSize = 24.sp,
-//        fontWeight = FontWeight.ExtraBold,
-//        fontFamily = FontFamily.SansSerif,
-//        textDecoration = TextDecoration.combine(
-//            listOf(TextDecoration.LineThrough, TextDecoration.Underline)
-//        ),
-
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = null,
+        modifier = Modifier.clip(CircleShape),
+        contentScale = ContentScale.Crop,
     )
 }
