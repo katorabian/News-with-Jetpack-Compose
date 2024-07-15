f35f70d1255d8c68ffcc2435e005a9e2aeab8128
katorabian <katorabian@gmail.com>
2024-07-15 19:03:23 +0700

    update(compose): provide colors depends on system theme

--- app/src/main/java/com/katorabian/compose_news/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/katorabian/compose_news/ui/theme/InstagramProfileCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,16 +22,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun InstagramProfileCard() {
     Card(
+        modifier = Modifier.padding(8.dp),
         shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp,),
-        border = BorderStroke(width = 1.dp, color = Color.Black),
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.onBackground),
         colors = CardColors(
-            containerColor = Color.White,
-            contentColor = Color.Transparent,
-            Color.White, Color.Transparent, ),
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            disabledContainerColor = MaterialTheme.colorScheme.background,
+            disabledContentColor = MaterialTheme.colorScheme.onBackground
+        ),
     ) {
         Row(
             modifier = Modifier
@@ -65,5 +68,21 @@ private fun TwoLines() {
             .size(25.dp)
             .background(color = Color.DarkGray)
         )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    ComposeNewsTheme(darkTheme = false) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    ComposeNewsTheme(darkTheme = true) {
+        InstagramProfileCard()
     }
 }
