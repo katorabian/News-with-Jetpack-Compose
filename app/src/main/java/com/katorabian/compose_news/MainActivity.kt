1fc63a2d66f1b560734efe61c3c7db85fb37c5a7
katorabian <katorabian@gmail.com>
2024-07-23 01:14:52 +0700

    feat(compose): try NavigationBar fun

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -60,6 +65,40 @@ fun Test() {
                         }
                     }
                 )
+            },
+            bottomBar = {
+                NavigationBar {
+                    NavigationBarItem(
+                        selected = true,
+                        onClick = {  },
+                        icon = {
+                            Icon(Icons.Filled.Favorite, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Favorite")
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {  },
+                        icon = {
+                            Icon(Icons.Outlined.Call, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Call")
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {  },
+                        icon = {
+                            Icon(Icons.Outlined.Add, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = "Add")
+                        }
+                    )
+                }
             }
         ) {
             Text(
