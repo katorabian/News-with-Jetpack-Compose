ef859aed0dd231f965c34a3f11695c5694ee6e02
katorabian <katorabian@gmail.com>
2024-07-26 17:22:33 +0700

    feat(compose): meet with LazyColumn

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.katorabian.compose_news
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
 import com.katorabian.compose_news.ui.theme.ComposeNewsTheme
 import com.katorabian.compose_news.ui.theme.InstagramProfileCard
 
@@ -43,9 +46,27 @@ private fun InstagramTest(viewModel: InstagramViewModel) {
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background),
         ) {
-            val scrollState = rememberScrollState()
-            Column(modifier = Modifier.verticalScroll(scrollState)) {
-                repeat(100) {
+            LazyColumn {
+                item {
+                    Text(
+                        text = "Hello user!",
+                        fontSize = 24.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                items(10) {
+                    Log.d("LAZY_LIST_TEST", "Card: Part 1, Number: $it")
+                    InstagramProfileCard(viewModel)
+                }
+                item {
+                    Log.d("LAZY_LIST_TEST", "Image")
+                    Image(
+                        painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+                        contentDescription = null
+                    )
+                }
+                items(200) {
+                    Log.d("LAZY_LIST_TEST", "Card: Part 2, Number: $it")
                     InstagramProfileCard(viewModel)
                 }
             }
