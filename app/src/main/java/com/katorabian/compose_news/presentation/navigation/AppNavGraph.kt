66a6501c4b21f22e14a88732e9d43e50122cc912
katorabian <katorabian@gmail.com>
2024-08-04 14:56:45 +0700

    feat(compose): create inner navGraph

--- app/src/main/java/com/katorabian/compose_news/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/katorabian/compose_news/presentation/navigation/AppNavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.get
 
 @Composable
@@ -13,14 +14,23 @@ fun AppNavGraph(
     navHostController: NavHostController,
     homeScreenContent: @Composable () -> Unit,
     favoriteScreenContent: @Composable () -> Unit,
-    profileScreenContent: @Composable () -> Unit
+    profileScreenContent: @Composable () -> Unit,
+    commentsScreenContent: @Composable () -> Unit
 ) {
     NavHost(
         navController = navHostController,
         startDestination = Screen.NewsFeed.route
     ) {
-        composable(route = Screen.NewsFeed.route) {
-            homeScreenContent()
+        navigation(
+            startDestination = Screen.NewsFeed.route,
+            route = Screen.Home.route
+        ) {
+            composable(route = Screen.NewsFeed.route) {
+                homeScreenContent()
+            }
+            composable(route = Screen.Comments.route) {
+                commentsScreenContent()
+            }
         }
         composable(route = Screen.Favourite.route) {
             favoriteScreenContent()
--- app/src/main/java/com/katorabian/compose_news/presentation/navigation/Screen.kt
+++ app/src/main/java/com/katorabian/compose_news/presentation/navigation/Screen.kt
@@ -2,12 +2,19 @@ package com.katorabian.compose_news.presentation.navigation
 
 sealed class Screen (val route: String) {
 
+    data object Home: Screen(ROUTE_HOME)
     data object NewsFeed: Screen(ROUTE_NEWS_FEED)
+    data object Comments: Screen(ROUTE_COMMENTS)
+
     data object Favourite: Screen(ROUTE_FAVOURITE)
     data object Profile: Screen(ROUTE_PROFILE)
 
     private companion object {
+
+        const val ROUTE_HOME = "home"
+        const val ROUTE_COMMENTS = "route_comments"
         const val ROUTE_NEWS_FEED = "news_feed"
+
         const val ROUTE_FAVOURITE = "favourite"
         const val ROUTE_PROFILE = "profile"
     }
