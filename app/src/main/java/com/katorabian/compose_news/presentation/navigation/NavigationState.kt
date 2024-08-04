3c56331d4bc365cad37a091158aacf7490df97be
katorabian <katorabian@gmail.com>
2024-08-04 16:11:19 +0700

    fix(compose): safe popupTo startDestination

--- app/src/main/java/com/katorabian/compose_news/presentation/navigation/NavigationState.kt
+++ app/src/main/java/com/katorabian/compose_news/presentation/navigation/NavigationState.kt
@@ -2,6 +2,7 @@ package com.katorabian.compose_news.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -11,7 +12,7 @@ class NavigationState(
 
     fun navigateTo(route: String) {
         navHostController.navigate(route) {
-            popUpTo(navHostController.graph.startDestinationId) { // remove screens between
+            popUpTo(navHostController.graph.findStartDestination().id) { // remove screens between
                 saveState = true // save state of removed screens
             }
             launchSingleTop = true // do not make new copy of already exits screen
