f0c5d792e5df1c3e2ad74ec8d05d6aa58f793fa9
katorabian <katorabian@gmail.com>
2024-08-05 18:33:48 +0700

    fix(compose): navArg navType

--- app/src/main/java/com/katorabian/compose_news/presentation/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/katorabian/compose_news/presentation/navigation/HomeScreenNavGraph.kt
@@ -2,8 +2,10 @@ package com.katorabian.compose_news.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import androidx.navigation.navArgument
 import com.katorabian.compose_news.domain.constant.ZERO_INT
 import com.katorabian.compose_news.domain.model.FeedPostItem
 
@@ -18,7 +20,12 @@ fun NavGraphBuilder.homeScreenNavGraph(
         composable(route = Screen.NewsFeed.route) {
             newsFeedScreenContent()
         }
-        composable(route = Screen.Comments.route) { // "comments/{feed_post_id}"
+        composable(
+            route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_FEED_POST_ID) { type = NavType.IntType }
+            )
+        ) { // "comments/{feed_post_id}"
             val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID)?: ZERO_INT
             commentsScreenContent(FeedPostItem(id = feedPostId))
         }
