1a228118333bdd3124ca8df986e4ddd5af4ee6aa
katorabian <katorabian@gmail.com>
2024-08-08 00:00:18 +0700

    update(vk-sdk): get sdk token by self instead of simple lib fun

--- app/src/main/java/com/katorabian/compose_news/presentation/screen/main/LoginViewModel.kt
+++ app/src/main/java/com/katorabian/compose_news/presentation/screen/main/LoginViewModel.kt
@@ -1,20 +1,28 @@
 package com.katorabian.compose_news.presentation.screen.main
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.katorabian.compose_news.presentation.screen.main.models.AuthState
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
+import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthenticationResult
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel(application: Application): AndroidViewModel(application) {
 
     private val _authState = MutableLiveData<AuthState>(AuthState.Initial)
     val authState: LiveData<AuthState> = _authState
 
     init {
-        _authState.value = if (VK.isLoggedIn()) {
+        val storage = VKPreferencesKeyValueStorage(application)
+        val token = VKAccessToken.restore(storage)
+        val isLoggedIn = token != null && token.isValid
+
+        _authState.value = if (isLoggedIn) {
             AuthState.Authorized
         } else {
             AuthState.Unauthorized
