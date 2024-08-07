c286a1d4a992893d0b28153e96704d4d80fe8a96
katorabian <katorabian@gmail.com>
2024-08-07 18:41:42 +0700

    test(compose): try LaunchedEffect as another way to init side effect
    - Executes at first call or when key is changed

--- app/src/main/java/com/katorabian/compose_news/MainActivity.kt
+++ app/src/main/java/com/katorabian/compose_news/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.katorabian.compose_news.other.ActivityResultTest
 import com.katorabian.compose_news.other.InstagramViewModel
 import com.katorabian.compose_news.other.MyNumber
@@ -18,6 +23,7 @@ import com.vk.api.sdk.VK
 import com.vk.api.sdk.VK.getVKAuthActivityResultContract
 import com.vk.api.sdk.auth.VKAuthenticationResult
 import com.vk.api.sdk.auth.VKScope
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     private val instagramViewModel by viewModels<InstagramViewModel>()
@@ -34,22 +40,33 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ComposeNewsTheme {
+                val someState = remember {
+                    mutableStateOf(true)
+                }
+
+                Log.e("MainActivity", "Recomposition: ${someState.value}")
                 val launcher = rememberLauncherForActivityResult(
                     contract = getVKAuthActivityResultContract()
                 ) {
                     when (it) {
                         is VKAuthenticationResult.Success -> {
-                            Log.d("MainActivity", "Success auth")
+                            Log.e("MainActivity", "Success auth")
                         }
                         is VKAuthenticationResult.Failed -> {
                             Log.e("MainActivity", "Failed auth")
                         }
                     }
                 }
-                SideEffect {
-                    launcher.launch(listOf(VKScope.WALL))
+                LaunchedEffect(key1 = someState.value) { //executes at first call or when key is changed
+                    Log.e("MainActivity", "LaunchedEffect")
+                    delay(100)
                 }
-                MainScreen()
+                SideEffect {
+                    Log.d("MainActivity", "SideEffect")
+                }
+                Button(onClick = { someState.value = !someState.value }) {
+                    Text(text = "Update state")
+                }
             }
         }
     }
