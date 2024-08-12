cee0267af878eb8d66d4d64e164a21fbca43d4ab
katorabian <katorabian@gmail.com>
2024-08-13 01:31:51 +0700

    practice(build): try different init of cold flow

--- practice/src/main/java/com/katorabian/practice/flow/lesson2/Lesson2.kt
+++ practice/src/main/java/com/katorabian/practice/flow/lesson2/Lesson2.kt
@@ -1,14 +1,16 @@
 package com.katorabian.practice.flow.lesson2
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 
 suspend fun main() {
-    val numbers = listOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61).asFlow()
-
-    numbers
+    getFlowByBuilder()
         .filter { it.isPrime() }
         .filter { it > 20 }
         .map {
@@ -16,6 +18,26 @@ suspend fun main() {
             "Number: $it"
         }
         .collect { println(it) }
+}
+
+fun getFlowByFlowOfBuilder(): Flow<Int> {
+    return flowOf<Int>(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+}
+
+fun getFlowByBuilder(): Flow<Int> {
+    val numbers = listOf(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+    return flow {
+        numbers.forEach {
+            emit(it)
+        }
+        val a = 43
+        emit(a)
+        println("Emitted: $a")
+        delay(1000)
+        val b = a * 10
+        emit(b)
+        println("Emitted: $b")
+    }
 }
 
 suspend fun Int.isPrime(): Boolean {
