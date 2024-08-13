cc69f79f79f58a209dd3ee58df1c6e536930d00f
katorabian <katorabian@gmail.com>
2024-08-13 10:47:58 +0700

    practice(build): emit one flow data to another flow

---
+++ practice/src/main/java/com/katorabian/practice/flow/lesson3/Lesson3.kt
@@ -0,0 +1,45 @@
+package com.katorabian.practice.flow.lesson3
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+
+suspend fun main() {
+    getFlowByBuilder()
+        .filter { it.isPrime() }
+        .filter { it > 20 }
+        .map {
+            println("Map")
+            "Number: $it"
+        }
+        .collect { println(it) }
+}
+
+fun getFlowByFlowOfBuilder(): Flow<Int> {
+    return flowOf<Int>(3, 4, 8, 16, 5, 7, 11, 32, 41, 28, 43, 47, 84, 116, 53, 59, 61)
+}
+
+fun getFlowByBuilder(): Flow<Int> {
+    val firstFlow = getFlowByFlowOfBuilder()
+    return flow {
+//        firstFlow.collect {
+//            println("Emitted from first flow: $it")
+//            emit(it)
+//        }
+        emitAll(firstFlow)
+    }
+}
+
+suspend fun Int.isPrime(): Boolean {
+    if (this <= 1) return false
+    for (i in 2 .. this / 2) {
+        delay(50)
+        if (this % i == 0) return false
+    }
+    return true
+}
