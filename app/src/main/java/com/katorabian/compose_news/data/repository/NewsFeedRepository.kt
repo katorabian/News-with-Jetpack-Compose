6eed5212cfe215cf1ed8db6ea8a609ec1922a3e6
katorabian <katorabian@gmail.com>
2024-08-14 18:13:39 +0700

    fix(network): request postFeed crash in flow

--- app/src/main/java/com/katorabian/compose_news/data/repository/NewsFeedRepository.kt
+++ app/src/main/java/com/katorabian/compose_news/data/repository/NewsFeedRepository.kt
@@ -11,12 +11,15 @@ import com.vk.api.sdk.VKPreferencesKeyValueStorage
 import com.vk.api.sdk.auth.VKAccessToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.stateIn
 
 class NewsFeedRepository(application: Application) {
@@ -47,6 +50,9 @@ class NewsFeedRepository(application: Application) {
             _feedPosts.addAll(posts)
             emit(feedPosts)
         }
+    }.retry {
+        delay(RETRY_TIMEOUT_MILLIS)
+        true
     }
 
 
@@ -119,5 +125,9 @@ class NewsFeedRepository(application: Application) {
         )
         _feedPosts.removeIf { it.id == feedPost.id }
         refreshedListFlow.emit(feedPosts)
+    }
+
+    companion object {
+        private const val RETRY_TIMEOUT_MILLIS = 3_000L
     }
 }
