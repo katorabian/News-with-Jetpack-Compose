d11d0408077e06ab3435f75c5f8f4d5624f31f16
katorabian <katorabian@gmail.com>
2024-08-15 21:05:55 +0700

    update(dagger): add @Provides for create repositories dependencies injection

--- app/src/main/java/com/katorabian/compose_news/di/DataModule.kt
+++ app/src/main/java/com/katorabian/compose_news/di/DataModule.kt
@@ -1,13 +1,18 @@
 package com.katorabian.compose_news.di
 
+import android.content.Context
+import com.katorabian.compose_news.data.network.VkApi
+import com.katorabian.compose_news.data.network.VkApiFactory
 import com.katorabian.compose_news.data.repository.AuthRepositoryImpl
 import com.katorabian.compose_news.data.repository.CommentsRepositoryImpl
 import com.katorabian.compose_news.data.repository.NewsFeedRepositoryImpl
 import com.katorabian.compose_news.domain.repository.AuthRepository
 import com.katorabian.compose_news.domain.repository.CommentsRepository
 import com.katorabian.compose_news.domain.repository.NewsFeedRepository
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 @Module
 interface DataModule {
@@ -18,4 +23,19 @@ interface DataModule {
     fun bindNewsFeedRepository(impl: NewsFeedRepositoryImpl): NewsFeedRepository
     @Binds
     fun bindCommentsRepository(impl: CommentsRepositoryImpl): CommentsRepository
+
+
+    companion object {
+        @Provides
+        fun provideApiService(): VkApi {
+            return VkApiFactory.apiService
+        }
+
+        @Provides
+        fun provideVkStorage(
+            context: Context
+        ): VKPreferencesKeyValueStorage {
+            return VKPreferencesKeyValueStorage(context)
+        }
+    }
 }
