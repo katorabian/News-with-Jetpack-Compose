a6667f2fe72b4ab4c37860a86c05acbba1fc8527
katorabian <katorabian@gmail.com>
2024-08-15 21:05:08 +0700

    update(dagger): add @Binds for create repositories implementation

--- app/src/main/java/com/katorabian/compose_news/di/DataModule.kt
+++ app/src/main/java/com/katorabian/compose_news/di/DataModule.kt
@@ -1,8 +1,21 @@
 package com.katorabian.compose_news.di
 
+import com.katorabian.compose_news.data.repository.AuthRepositoryImpl
+import com.katorabian.compose_news.data.repository.CommentsRepositoryImpl
+import com.katorabian.compose_news.data.repository.NewsFeedRepositoryImpl
+import com.katorabian.compose_news.domain.repository.AuthRepository
+import com.katorabian.compose_news.domain.repository.CommentsRepository
+import com.katorabian.compose_news.domain.repository.NewsFeedRepository
+import dagger.Binds
 import dagger.Module
 
 @Module
 interface DataModule {
-    
+
+    @Binds
+    fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
+    @Binds
+    fun bindNewsFeedRepository(impl: NewsFeedRepositoryImpl): NewsFeedRepository
+    @Binds
+    fun bindCommentsRepository(impl: CommentsRepositoryImpl): CommentsRepository
 }
