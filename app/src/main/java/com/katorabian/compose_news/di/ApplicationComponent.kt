5ba9ca447ed9f05339f4dc79fd93003eba1ede60
katorabian <katorabian@gmail.com>
2024-08-15 21:04:12 +0700

    update(dagger): create @Component & @Module

---
+++ app/src/main/java/com/katorabian/compose_news/di/ApplicationComponent.kt
@@ -0,0 +1,8 @@
+package com.katorabian.compose_news.di
+
+import dagger.Component
+
+@Component
+interface ApplicationComponent {
+
+}
---
+++ app/src/main/java/com/katorabian/compose_news/di/DataModule.kt
@@ -0,0 +1,8 @@
+package com.katorabian.compose_news.di
+
+import dagger.Module
+
+@Module
+interface DataModule {
+    
+}
