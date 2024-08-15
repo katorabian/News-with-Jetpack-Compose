68e5fa79f16c23a0ce6a21f8e9edf153c441d027
katorabian <katorabian@gmail.com>
2024-08-15 21:08:16 +0700

    update(dagger): add @Component.Factory for provide @BindsInstance (basic dependencies)

--- app/src/main/java/com/katorabian/compose_news/di/ApplicationComponent.kt
+++ app/src/main/java/com/katorabian/compose_news/di/ApplicationComponent.kt
@@ -1,8 +1,21 @@
 package com.katorabian.compose_news.di
 
+import android.content.Context
+import dagger.BindsInstance
 import dagger.Component
 
 @Component
 interface ApplicationComponent {
 
+    @Component.Factory
+    interface Factory { // for providing all dependencies we need to create @Component
+
+        fun create(
+            @BindsInstance context: Context //in current case we need just 1 parameter for @Component creation
+        ): ApplicationComponent
+    }
 }
+
+// При создании ApplicationComponent мы передадим Context
+// он будет добавлен в граф зависимостей
+// и будет передан в DataModule и предоставлен во всех необходимых методах для создания зависимостей
