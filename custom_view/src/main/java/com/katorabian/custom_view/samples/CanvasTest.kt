576802fb790c2ad08c56e14748fe3b34e31532b5
katorabian <katorabian@gmail.com>
2024-08-20 23:28:38 +0700

    update(customView): drawCircle practice

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.drawscope.Stroke
 
 @Composable
 fun CanvasTest() {
@@ -18,25 +18,20 @@ fun CanvasTest() {
     ) {
         drawLine(
             color = Color.White,
-            start = Offset(100F, 100F),
-            end = Offset(200F, 100F),
-            strokeWidth = 2.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(100F, 100F),
-            end = Offset(100F, 300F),
-            strokeWidth = 2.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
             start = Offset(0F, 0F),
-            end = Offset(size.width, size.height)
+            end = Offset(size.width, size.height),
+            strokeWidth = 1F
         )
         drawLine(
             color = Color.White,
             start = Offset(0F, size.height),
-            end = Offset(size.width, 0F)
+            end = Offset(size.width, 0F),
+            strokeWidth = 1F
+        )
+        drawCircle(
+            color = Color.White,
+            radius = 100F,
+            style = Stroke(width = 1F)
         )
     }
 }
