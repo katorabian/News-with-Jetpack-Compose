6d15879d4d056d0e30515c1d20fe41e00bf4387f
katorabian <katorabian@gmail.com>
2024-08-20 23:29:23 +0700

    update(customView): using dp instead of raw pixels

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CanvasTest() {
@@ -20,18 +21,18 @@ fun CanvasTest() {
             color = Color.White,
             start = Offset(0F, 0F),
             end = Offset(size.width, size.height),
-            strokeWidth = 1F
+            strokeWidth = 1.dp.toPx()
         )
         drawLine(
             color = Color.White,
             start = Offset(0F, size.height),
             end = Offset(size.width, 0F),
-            strokeWidth = 1F
+            strokeWidth = 1.dp.toPx()
         )
         drawCircle(
             color = Color.White,
-            radius = 100F,
-            style = Stroke(width = 1F)
+            radius = 100.dp.toPx(),
+            style = Stroke(width = 1.dp.toPx())
         )
     }
 }
