5573d787770c0ec3522d201bec2e0ce559332c49
katorabian <katorabian@gmail.com>
2024-08-20 23:22:59 +0700

    update(customView): drawLine practice

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CanvasTest() {
@@ -18,7 +19,24 @@ fun CanvasTest() {
         drawLine(
             color = Color.White,
             start = Offset(100F, 100F),
-            end = Offset(200F, 200F)
+            end = Offset(200F, 100F),
+            strokeWidth = 2.dp.toPx()
+        )
+        drawLine(
+            color = Color.White,
+            start = Offset(100F, 100F),
+            end = Offset(100F, 300F),
+            strokeWidth = 2.dp.toPx()
+        )
+        drawLine(
+            color = Color.White,
+            start = Offset(0F, 0F),
+            end = Offset(size.width, size.height)
+        )
+        drawLine(
+            color = Color.White,
+            start = Offset(0F, size.height),
+            end = Offset(size.width, 0F)
         )
     }
 }
