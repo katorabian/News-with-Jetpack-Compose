11fb1118452ff9195ae66200992a0ecf53fa6010
katorabian <katorabian@gmail.com>
2024-08-21 13:15:21 +0700

    update(customView): improvisation with drawHouse using Canvas

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.TileMode
@@ -21,6 +22,59 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun CanvasTest() {
+//    DrawStar()
+    DrawHouse()
+}
+
+@Preview
+@Composable
+fun DrawHouse() {
+    Canvas(
+        modifier = Modifier
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Magenta, Color.Blue)
+                )
+            ).fillMaxSize()
+    ) {
+        fun Int.px() = dp.toPx()
+        fun pxFromCenter(horizontal: Int) = center.x + horizontal.px()
+        drawPath(
+            path = Path().apply {
+                // roof
+                moveTo(center.x, 300.px())
+                lineTo(pxFromCenter(100), 400.px())
+                lineTo(pxFromCenter(-100), 400.px())
+                lineTo(center.x, 300.px())
+
+                // walls
+                moveTo(pxFromCenter(100), 400.px())
+                lineTo(pxFromCenter(100), 600.px())
+                lineTo(pxFromCenter(-100), 600.px())
+                lineTo(pxFromCenter(-100), 400.px())
+
+                // window
+                moveTo(pxFromCenter(-50), 450.px())
+                lineTo(pxFromCenter(50), 450.px())
+                lineTo(pxFromCenter(50), 550.px())
+                lineTo(pxFromCenter(-50), 550.px())
+                lineTo(pxFromCenter(-50), 450.px())
+
+                moveTo(pxFromCenter(-50), 500.px())
+                lineTo(pxFromCenter(50), 500.px())
+                moveTo(center.x, 450.px())
+                lineTo(center.x, 550.px())
+            },
+            color = Color.White,
+            style = Stroke(width = 4.px())
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun DrawStar() {
     Canvas(
         modifier = Modifier
             .fillMaxSize()
@@ -55,6 +109,7 @@ fun CanvasTest() {
             style = Fill
         )
     }
+
 }
 
 @Composable
