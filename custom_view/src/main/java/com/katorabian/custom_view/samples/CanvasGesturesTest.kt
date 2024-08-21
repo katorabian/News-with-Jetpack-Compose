5b6449ec5ddff6c8b01e702e408ee182b53e6eaa
katorabian <katorabian@gmail.com>
2024-08-21 16:55:29 +0700

    update(customView): simpled variant + Brush

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasGesturesTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasGesturesTest.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.random.Random
 
 @Preview
 @Composable
@@ -35,15 +34,12 @@ fun CanvasGesturesTest() {
                 }
             }
     ) {
-        circles.value.forEachIndexed { index, offset ->
+        circles.value.forEach { offset ->
             drawCircle(
-                color = Color(
-                    Random.nextInt(255),
-                    Random.nextInt(255),
-                    Random.nextInt(255),
-                    Random.nextInt(255)
+                brush = Brush.linearGradient(
+                    listOf(Color.Magenta, Color.Cyan, Color.Red)
                 ),
-                radius = index.dp.toPx(),
+                radius = 10.dp.toPx(),
                 center = offset
             )
         }
