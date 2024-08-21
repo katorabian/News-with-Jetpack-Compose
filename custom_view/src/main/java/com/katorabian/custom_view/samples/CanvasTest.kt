ec840bfbbe2b6e867169396dbd4508a12ea58f04
katorabian <katorabian@gmail.com>
2024-08-21 12:43:35 +0700

    update(customView): drawPath practice

--- custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
+++ custom_view/src/main/java/com/katorabian/custom_view/samples/CanvasTest.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,67 +22,24 @@ fun CanvasTest() {
             .fillMaxSize()
             .background(Color.Black)
     ) {
-        // "О"
-        drawCircle(
-            color = Color.Cyan,
-            radius = 20.dp.toPx(),
-            center = Offset(50.dp.toPx(), 50.dp.toPx()),
-            style = Stroke(width = 1.dp.toPx())
-        )
+        fun Int.px() = dp.toPx()
+        drawPath(
+            path = Path().apply {
+                moveTo(center.x, 100.px())
+                lineTo(center.x + 25.px(), 150.px())
+                lineTo(center.x + 75.px(), 150.px())
+                lineTo(center.x + 40.px(), 180.px())
+                lineTo(center.x + 65.px(), 240.px())
+                lineTo(center.x, 205.px())
 
-        // "Л"
-        drawLine(
-            color = Color.Magenta,
-            start = Offset(96.dp.toPx(), 30.dp.toPx()),
-            end = Offset(78.dp.toPx(), 70.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
+                lineTo(center.x - 65.px(), 240.px())
+                lineTo(center.x - 40.px(), 180.px())
+                lineTo(center.x - 75.px(), 150.px())
+                lineTo(center.x - 25.px(), 150.px())
+                lineTo(center.x, 100.px())
+            },
+            color = Color.White,
+            style = Fill
         )
-        drawLine(
-            color = Color.Magenta,
-            start = Offset(96.dp.toPx(), 30.dp.toPx()),
-            end = Offset(114.dp.toPx(), 70.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        // "Е"
-        drawLine(
-            color = Color.Yellow,
-            start = Offset(126.dp.toPx(), 30.dp.toPx()),
-            end = Offset(126.dp.toPx(), 70.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.Yellow,
-            start = Offset(126.dp.toPx(), 30.dp.toPx()),
-            end = Offset(150.dp.toPx(), 30.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.Yellow,
-            start = Offset(126.dp.toPx(), 50.dp.toPx()),
-            end = Offset(150.dp.toPx(), 50.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.Yellow,
-            start = Offset(126.dp.toPx(), 70.dp.toPx()),
-            end = Offset(150.dp.toPx(), 70.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
-        // "Г"
-        drawLine(
-            color = Color.Red,
-            start = Offset(164.dp.toPx(), 30.dp.toPx()),
-            end = Offset(192.dp.toPx(), 30.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.Red,
-            start = Offset(164.dp.toPx(), 30.dp.toPx()),
-            end = Offset(164.dp.toPx(), 70.dp.toPx()),
-            strokeWidth = 1.dp.toPx()
-        )
-
     }
 }
