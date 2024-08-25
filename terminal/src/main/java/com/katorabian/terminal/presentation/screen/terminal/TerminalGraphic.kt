e177a1cf079c96f440f99550f89bdbd80012546f
katorabian <katorabian@gmail.com>
2024-08-25 20:11:04 +0700

    update(terminal): provide TransformableState for zoom gestures

--- terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
+++ terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
@@ -2,29 +2,47 @@ package com.katorabian.terminal.presentation.screen.terminal
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.katorabian.terminal.data.dto.BarDto
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun TerminalGraphic(
     bars: List<BarDto>
 ) {
+    var visibleBarsCount by remember {
+        mutableStateOf(100)
+    }
+
+    val transformableState = TransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .transformable(transformableState)
     ) {
         val max = bars.maxOf { it.high }
         val min = bars.minOf { it.low }
         val difference = max - min
-        val barWidth = size.width / bars.count()
+        val barWidth = size.width / visibleBarsCount
         val pxPerPoint = size.height / difference
-        bars.forEachIndexed { index, bar ->
+        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
             val offsetX = index * barWidth
             drawLine(
                 color = Color.White,
