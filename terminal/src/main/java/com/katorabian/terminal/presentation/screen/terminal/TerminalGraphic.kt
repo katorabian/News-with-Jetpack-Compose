72bda0ca5b66695fd441617f7134208f0b394f51
katorabian <katorabian@gmail.com>
2024-08-26 00:18:00 +0700

    feat(terminal): add horizontal scroll by TransformableState.panChange value & ::translate fun

--- terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
+++ terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
 import com.katorabian.terminal.data.dto.BarDto
 import kotlin.math.roundToInt
@@ -26,11 +27,16 @@ fun TerminalGraphic(
     var visibleBarsCount by remember {
         mutableStateOf(100)
     }
+    var scrolledBy by remember {
+        mutableStateOf(0F)
+    }
 
-    val transformableState = TransformableState { zoomChange, _, _ ->
+    val transformableState = TransformableState { zoomChange, panChange, _ ->
         val newCount = (visibleBarsCount / zoomChange).roundToInt()
         visibleBarsCount = newCount
             .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
+
+        scrolledBy += panChange.x
     }
 
     Canvas(
@@ -44,20 +50,22 @@ fun TerminalGraphic(
         val difference = max - min
         val barWidth = size.width / visibleBarsCount
         val pxPerPoint = size.height / difference
-        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
-            val offsetX = size.width - index * barWidth
-            drawLine(
-                color = Color.White,
-                start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
-                end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
-                strokeWidth = 1F.toDp().toPx()
-            )
-            drawLine(
-                color = bar.getColor(),
-                start = Offset(offsetX, size.height - ((bar.open - min) * pxPerPoint)),
-                end = Offset(offsetX, size.height - ((bar.close - min) * pxPerPoint)),
-                strokeWidth = barWidth / 2
-            )
+        translate(left = scrolledBy) {
+            bars.take(visibleBarsCount).forEachIndexed { index, bar ->
+                val offsetX = size.width - index * barWidth
+                drawLine(
+                    color = Color.White,
+                    start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.high - min) * pxPerPoint)),
+                    strokeWidth = 1F.toDp().toPx()
+                )
+                drawLine(
+                    color = bar.getColor(),
+                    start = Offset(offsetX, size.height - ((bar.open - min) * pxPerPoint)),
+                    end = Offset(offsetX, size.height - ((bar.close - min) * pxPerPoint)),
+                    strokeWidth = barWidth / 2
+                )
+            }
         }
     }
 }
