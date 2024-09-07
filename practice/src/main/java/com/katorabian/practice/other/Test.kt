acda63c5238a66061e0925e2165665a9d7e00c11
katorabian <katorabian@gmail.com>
2024-09-08 00:29:29 +0700

    practice(terminal): make not-bundle-compatible state possible to save after rotate

--- practice/src/main/java/com/katorabian/practice/other/Test.kt
+++ practice/src/main/java/com/katorabian/practice/other/Test.kt
@@ -1,5 +1,6 @@
 package com.katorabian.practice.other
 
+import android.os.Parcelable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,16 +8,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.parcelize.Parcelize
 
 @Preview
 @Composable
 fun Test() {
-    var testData by remember {
+    var testData by rememberSaveable {
         mutableStateOf(TestData(0))
     }
 
@@ -30,4 +32,5 @@ fun Test() {
     }
 }
 
-data class TestData(val number: Int)
+@Parcelize
+data class TestData(val number: Int): Parcelable
