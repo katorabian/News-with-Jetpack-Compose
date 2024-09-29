401180cc26884268e2b5739818d077f43edb7534
katorabian <katorabian@gmail.com>
2024-09-29 17:16:52 +0700

    refactor(terminal): extract duplicate ::drawText into ::drawTextPrice fun

--- terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
+++ terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalGraphic.kt
@@ -109,12 +109,50 @@ private fun DrawScope.drawPrices(
     textMeasurer: TextMeasurer
 ) {
     // max price
+    val maxPriceOffsetY = 0F // y = size.height - ((max - min) * pxPerPoint)
     drawDashLine(
-        start = Offset(0F, 0F), // y = size.height - ((max - min) * pxPerPoint)
-        end = Offset(size.width, 0F)
+        start = Offset(0F, maxPriceOffsetY),
+        end = Offset(size.width, maxPriceOffsetY)
     )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = max,
+        offsetY = maxPriceOffsetY
+    )
+
+    // last price
+    val lastPriceOffsetY = size.height - ((lastPrice - min) * pxPerPoint)
+    drawDashLine(
+        start = Offset(0F, lastPriceOffsetY),
+        end = Offset(size.width, lastPriceOffsetY),
+    )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = lastPrice,
+        offsetY = lastPriceOffsetY
+    )
+
+    // min price
+    val minPriceOffsetY = size.height // y = size.height - ((min - min) * pxPerPoint)
+    drawDashLine(
+        start = Offset(0F, minPriceOffsetY),
+        end = Offset(size.width, minPriceOffsetY),
+    )
+    drawTextPrice(
+        textMeasurer = textMeasurer,
+        price = min,
+        offsetY = minPriceOffsetY
+    )
+}
+
+private fun DrawScope.drawTextPrice(
+    textMeasurer: TextMeasurer,
+    price: Float,
+    offsetY: Float
+) {
+
     val textLayoutResult = textMeasurer.measure(
-        text = max.toString(),
+        text = price.toString(),
         style = TextStyle(
             color = Color.White,
             fontSize = 12.sp
@@ -122,19 +160,7 @@ private fun DrawScope.drawPrices(
     )
     drawText(
         textLayoutResult = textLayoutResult,
-        topLeft = Offset(size.width - textLayoutResult.size.width, 0F)
-    )
-
-    // last price
-    drawDashLine(
-        start = Offset(0F, size.height - ((lastPrice - min) * pxPerPoint)),
-        end = Offset(size.width, size.height - ((lastPrice - min) * pxPerPoint)),
-    )
-
-    // min price
-    drawDashLine(
-        start = Offset(0F, size.height), // y = size.height - ((min - min) * pxPerPoint)
-        end = Offset(size.width, size.height),
+        topLeft = Offset(size.width - textLayoutResult.size.width, offsetY)
     )
 }
 
