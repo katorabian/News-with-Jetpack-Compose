44578a7769e75654525ae6e715c12e73c49b93d6
katorabian <katorabian@gmail.com>
2025-04-08 16:52:25 +0700

    feat(practice): first try for many months

--- terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalScreen.kt
+++ terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalScreen.kt
@@ -1,17 +1,69 @@
 package com.katorabian.terminal.presentation.screen.terminal
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Call
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.DrawerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.katorabian.terminal.R
+import com.katorabian.terminal.presentation.ui.theme.TerminalTheme
 
 @Preview
 @Composable
@@ -35,6 +87,67 @@ fun TerminalScreen() {
 
         is TerminalScreenState.Initial -> {
 
+        }
+    }
+}
+
+@Composable
+@Preview
+fun NewsListItem() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        border = BorderStroke(1.dp, color = Color.Green)
+    ) {
+        Column(
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max), // ключевой момент
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(Color.Black),
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+                Spacer(Modifier.width(8.dp))
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Title")
+                    Text(text = "Description")
+                }
+            }
+            Spacer(Modifier.height(4.dp))
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                modifier = Modifier
+                    .aspectRatio(1F)
+                    .fillMaxWidth(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(4.dp))
+            Text(
+                buildAnnotatedString {
+                    append("Жизнь в зелёном квадрате\n")
+                    withStyle(
+                        SpanStyle(
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    ) {
+                        append("#Котики #Пчёлки #Красноярск")
+                    }
+                }
+            )
         }
     }
 }
