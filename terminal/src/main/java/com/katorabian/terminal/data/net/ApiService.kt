db10dc09f5ab5e1f4592e1927781cc52e71b3796
katorabian <katorabian@gmail.com>
2025-06-04 14:09:50 +0700

    refactor(terminal): use dynamic dates instead of hardcode

--- terminal/src/main/java/com/katorabian/terminal/data/net/ApiService.kt
+++ terminal/src/main/java/com/katorabian/terminal/data/net/ApiService.kt
@@ -2,9 +2,13 @@ package com.katorabian.terminal.data.net
 
 import com.katorabian.terminal.data.dto.BarsResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
-    @GET("./v2/aggs/ticker/AAPL/range/1/hour/2023-01-09/2025-02-10?adjusted=true&sort=desc&limit=50000&apiKey=kNTpR0PcNe8XWT9t5TkK88I0LZHoYtAN")
-    suspend fun loadBars(): BarsResponse
+    @GET("./v2/aggs/ticker/AAPL/range/1/hour/{from}/{to}?adjusted=true&sort=desc&limit=50000&apiKey=kNTpR0PcNe8XWT9t5TkK88I0LZHoYtAN")
+    suspend fun loadBars(
+        @Path("from") from: String,
+        @Path("to") to: String
+    ): BarsResponse
 }
--- terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalViewModel.kt
+++ terminal/src/main/java/com/katorabian/terminal/presentation/screen/terminal/TerminalViewModel.kt
@@ -1,5 +1,6 @@
 package com.katorabian.terminal.presentation.screen.terminal
 
+import android.os.Build
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,6 +9,14 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class TerminalViewModel: ViewModel() {
 
@@ -30,8 +39,39 @@ class TerminalViewModel: ViewModel() {
 
     private fun loadBarList() {
         viewModelScope.launch(exceptionHandler) {
-            val barList = apiService.loadBars().barList
+            val range = get2YearsRange()
+            val barList = apiService.loadBars(range.first, range.second).barList
             _state.value = TerminalScreenState.Content(barList = barList)
         }
+    }
+
+    private fun get2YearsRange(): Pair<String, String> {
+        val today = getCurrentDate()
+        val dateFormat = SimpleDateFormat(API_DATE_PATTERN, Locale.UK)
+        val to = dateFormat.format(today)
+        val from = dateFormat.minus2Years(today)
+        return from to to
+    }
+
+    private fun DateFormat.minus2Years(initialDate: Date): String {
+        val calendar = Calendar.getInstance()
+        calendar.time = initialDate
+        val year = calendar.get(Calendar.YEAR) - YEARS_RANGE
+        calendar.set(Calendar.YEAR, year)
+        return format(calendar.time)
+    }
+
+    private fun getCurrentDate() = run {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val instant = ZonedDateTime.now(ZoneOffset.UTC).toInstant()
+            Date.from(instant)
+        } else {
+            Calendar.getInstance(TimeZone.getTimeZone("UTC")).time
+        }
+    }
+
+    companion object {
+        private const val API_DATE_PATTERN = "yyyy-MM-dd"//2023-01-09
+        private const val YEARS_RANGE = 2
     }
 }
