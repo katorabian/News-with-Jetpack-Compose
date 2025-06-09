eeee96fed9cffcce2f6615bf3a09523e17454d8b
katorabian <katorabian@gmail.com>
2025-06-09 10:19:20 +0700

    feat(decompose): declare AddContactComponent interface and implementation

---
+++ mvidecomposetest/src/main/java/com/katorabian/mvidecomposetest/presentation/AddContactComponent.kt
@@ -0,0 +1,17 @@
+package com.katorabian.mvidecomposetest.presentation
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface AddContactComponent {
+
+    val model: StateFlow<Model>
+
+    fun onUsernameChanged(username: String)
+    fun onPhoneChanged(phone: String)
+    fun onSaveContactClicked()
+
+    data class Model(
+        val username: String,
+        val phone: String
+    )
+}
